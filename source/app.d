10f6c189d73156edd4f2c4038f5ada8f8320f5d0
outland.karasu <outland.karasu@gmail.com>
2021-02-15 08:36:06 +0900

    add karasux.

--- source/app.d
+++ source/app.d
@@ -1,5 +1,22 @@
 import std.stdio;
 
+struct KalmanFilter(T = real)
+{
+    T opCall(scope auto ref const(T) x) nothrow pure @safe
+    {
+        state_ = drift_ + trend_ * state_ + error(stateVarience_);
+        return constant_ + state_ * x + error(errorVarience_);
+    }
+
+private:
+    T drift_;
+    T trend_;
+    T constant_;
+    T errorVariance_;
+    T stateVarience_;
+    T state_;
+}
+
 void main()
 {
 	writeln("Edit source/app.d to start your project.");
